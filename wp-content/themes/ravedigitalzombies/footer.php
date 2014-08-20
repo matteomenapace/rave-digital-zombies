78d22b5b72ee9302a3287cb68742944c8feb66ff
Matteo Menapace <hello@baddeo.com>
2014-08-20 23:51:11 +0100

    Added "baddeo" stylesheet to align the look&feel of this microsite to Rave's official website

--- wp-content/themes/ravedigitalzombies/footer.php
+++ wp-content/themes/ravedigitalzombies/footer.php
@@ -1,7 +1,7 @@
 			<footer role="contentinfo">
 			
 				<div id="inner-footer" class="clearfix">
-		          <hr />
+		          <!-- <hr /> -->
 		          <div id="widget-footer" class="clearfix row">
 		            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer1') ) : ?>
 		            <?php endif; ?>
@@ -15,15 +15,17 @@
 						<?php wp_bootstrap_footer_links(); // Adjust using Menus in Wordpress Admin ?>
 					</nav>
 					
-					<p class="pull-right">Developed by <a href="http://www.ravensbourne.ac.uk/courses/undergraduate-2014-2015/web-media/ba-%28hons%29-web-media/" target="_blank">Web Media Students</a> at Ravensbourne.</p>
+					<!-- <p class="pull-right">Developed by <a href="http://www.ravensbourne.ac.uk/courses/undergraduate-2014-2015/web-media/ba-%28hons%29-web-media/" target="_blank">Web Media Students</a> at Ravensbourne.</p> -->
 			
-					<p class="attribution">&copy; <?php bloginfo('name'); ?></p>
+					<!-- <p class="attribution">&copy; <?php bloginfo('name'); ?></p> -->
 				
 				</div> <!-- end #inner-footer -->
 				
 			</footer> <!-- end footer -->
 		
 		</div> <!-- end #container -->
+
+		<a href="http://www.ravensbourne.ac.uk/" class="logo">Ravensbourne</a>
 				
 		<!--[if lt IE 7 ]>
   			<script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
