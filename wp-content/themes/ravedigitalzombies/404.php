beeef938c2cc3feb7679618f151052bad41008ea
Selcuk Cura <selcukcura@gmail.com>
2014-08-22 15:45:22 +0100

    responsive fixes, styled 404 page

--- wp-content/themes/ravedigitalzombies/404.php
+++ wp-content/themes/ravedigitalzombies/404.php
@@ -1,42 +1,27 @@
 <?php get_header(); ?>
-			
-			<div id="content" class="clearfix row">
-			
-				<div id="main" class="col-sm-12 clearfix" role="main">
 
-					<article id="post-not-found" class="clearfix">
-						
-						<header>
-
-							<div class="hero-unit">
-							
-								<h1><?php _e("Epic 404 - Article Not Found","wpbootstrap"); ?></h1>
-								<p><?php _e("This is embarassing. We can't find what you were looking for.","wpbootstrap"); ?></p>
-															
-							</div>
-													
-						</header> <!-- end article header -->
-					
-						<section class="post_content">
-							
-							<p><?php _e("Whatever you were looking for was not found, but maybe try looking again or search using the form below.","wpbootstrap"); ?></p>
-
-							<div class="row">
-								<div class="col col-lg-12">
-									<?php get_search_form(); ?>
-								</div>
-							</div>
-					
-						</section> <!-- end article section -->
-						
-						<footer>
-							
-						</footer> <!-- end article footer -->
-					
-					</article> <!-- end article -->
-			
-				</div> <!-- end #main -->
-    
-			</div> <!-- end #content -->
+<div id="content" class="clearfix row">
+    <div id="main" class="col-sm-9 clearfix" role="main">
+        <article id="post-not-found" class="clearfix">
+            <div class="main-container">
+                <!-- col-sm-10  -->
+                <header>
+                    <h1 class="page-title"><?php _e("404: Page  Not Found","wpbootstrap"); ?></h1>
+                    <p>
+                        <?php _e( "This is embarassing. We can't find what you were looking for.", "wpbootstrap"); ?>
+                    </p>
+                    <button type="button" class="btn btn-primary green-button">Click here to go back home</button>
+                </header>
+                <!-- end article header -->
+        </article>
+        <!-- end article -->
+        </div>
+        <!-- end #main -->
+        <div class="col-sm-3 sidebar ">
+            <?php wp_bootstrap_main_nav(); // Adjust using Menus in Wordpress Admin ?>
+        </div>
+    </div>
+</div>
+<!-- end #content -->
 
 <?php get_footer(); ?>
