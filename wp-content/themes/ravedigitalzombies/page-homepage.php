64b5ae8a637ad80289f59afa6473fdba2556823d
Matteo Menapace <hello@baddeo.com>
2014-09-25 16:47:40 +0100

    Homepage with links

--- wp-content/themes/ravedigitalzombies/page-homepage.php
+++ wp-content/themes/ravedigitalzombies/page-homepage.php
@@ -1,82 +1,79 @@
 <?php
 /*
-Template Name: Homepage
+Template Name: Home
 */
 ?>
 
 <?php get_header(); ?>
-			
-			<div id="content" class="clearfix row">
-			
-				<div id="main" class="col-sm-12 clearfix" role="main">
 
-					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-					
-					<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
-					
-						<header>
+<div id="content" class="clearfix row">
 
-							<?php 
-								$post_thumbnail_id = get_post_thumbnail_id();
-								$featured_src = wp_get_attachment_image_src( $post_thumbnail_id, 'wpbs-featured-home' );
-							?>
+	<div id="main" class="col-sm-9 sidebar" role="main">
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		
+		<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
 
-							<div class="jumbotron" style="background-image: url('<?php echo $featured_src[0]; ?>'); background-repeat: no-repeat; background-position: 0 0;">
-				
-								<div class="page-header">
-									<h1><?php bloginfo('title'); ?><small><?php echo get_post_meta($post->ID, 'custom_tagline' , true);?></small></h1>
-								</div>				
-								
-							</div>
-						
-						</header>
-						
-						<section class="row post_content">
-						
-							<div class="col-sm-8">
-						
-								<?php the_content(); ?>
-								
-							</div>
-							
-							<?php get_sidebar('sidebar2'); // sidebar 2 ?>
-													
-						</section> <!-- end article header -->
-						
-						<footer>
+			<a href="#menu"><button class="navicon"><i class="fa fa-bars fa-lg"></i></button></a>
+
+			<div class="main-container">
+
+				<?php the_content(); ?>
+			   
+				<?php wp_bootstrap_main_nav(); // Adjust using Menus in Wordpress Admin ?>
+
+			</div> 
+									
+		</article> 
+					
+		<?php endwhile; ?>	
+		
+		<?php else : ?>
+					
+			<article id="post-not-found">
+				<header>
+					<h1><?php _e("Not Found", "wpbootstrap"); ?></h1>
+				</header>
+				<section class="post_content">
+					<p><?php _e("Sorry, but the requested resource was not found on this site.", "wpbootstrap"); ?></p>
+				</section>
+			</article>
 			
-							<p class="clearfix"><?php the_tags('<span class="tags">' . __("Tags","wpbootstrap") . ': ', ', ', '</span>'); ?></p>
-							
-						</footer> <!-- end article footer -->
-					
-					</article> <!-- end article -->
-					
-					<?php 
-						// No comments on homepage
-						//comments_template();
-					?>
-					
-					<?php endwhile; ?>	
-					
-					<?php else : ?>
-					
-					<article id="post-not-found">
-					    <header>
-					    	<h1><?php _e("Not Found", "wpbootstrap"); ?></h1>
-					    </header>
-					    <section class="post_content">
-					    	<p><?php _e("Sorry, but the requested resource was not found on this site.", "wpbootstrap"); ?></p>
-					    </section>
-					    <footer>
-					    </footer>
-					</article>
-					
-					<?php endif; ?>
-			
-				</div> <!-- end #main -->
-    
-				<?php //get_sidebar(); // sidebar 1 ?>
-    
-			</div> <!-- end #content -->
+		<?php endif; ?>
+
+	</div> <!-- end #main -->
+
+	<div id="menu" class="col-sm-3 sidebar">
+		<div class="twitter-gradient">
+
+			<a class="twitter-timeline" href="https://twitter.com/hashtag/ravedz" data-widget-id="502097294963904515" data-theme="light" data-link-color="#AA9C8F" data-related="twitterapi,twitter" data-aria-polite="assertive" width="300" height="300">#ravedz</a>
+			<script>
+				! function (d, s, id) {
+					var js, fjs = d.getElementsByTagName(s)[0],
+						p = /^http:/.test(d.location) ? 'http' : 'https';
+					if (!d.getElementById(id)) {
+						js = d.createElement(s);
+						js.id = id;
+						js.src = p + "://platform.twitter.com/widgets.js";
+						fjs.parentNode.insertBefore(js, fjs);
+					}
+				}(document, "script", "twitter-wjs");
+			</script>
+		</div>
+
+	</div>        
+
+</div> <!-- end #content -->
+
+<script type="text/javascript">
+
+	jQuery(document).ready(function($)
+	{
+		$('#menu-main-menu a').each(function()
+		{
+			$(this).attr('target', '_blank')
+		})
+	})
+
+</script>		
 
 <?php get_footer(); ?>
